86324e519430932c02fd6d3089c3bb330efed046
anastasiyache <kittydon777@gmail.com>
2018-02-18 00:25:09 +0300

    Commit 3

--- OpenWeatherMap.py
+++ OpenWeatherMap.py
@@ -8,20 +8,19 @@ observation = owm.weather_at_place('Rostov-on-Don,ru')
 weather = observation.get_weather()
 location = observation.get_location()
 
-print('Страна: ' + location.get_country())
-print('Город: ' + location.get_name())
-print('Долгота: ' + str(location.get_lon()))
-print('Широта: ' + str(location.get_lat()))
-print('Облачность: ' + str(weather.get_clouds()))
-print('Статус: ' + str(weather.get_detailed_status()))
-print('Давление: ' + str(weather.get_pressure()))
-print('Дождь: ' + str(weather.get_rain()))
-print('Снег: ' + str(weather.get_snow()))
-print('Время: ' + str(weather.get_reference_time('iso')))
-print('Статус: ' + str(weather.get_status()))
-print('Восход: ' + str(weather.get_sunrise_time('iso')))
-print('Закат: ' + str(weather.get_sunset_time('iso')))
-print('Температра: ' + str(weather.get_temperature('celsius')))
-print('Видимость: ' + str(weather.get_visibility_distance()))
-print('Изображение: ' + weather.get_weather_icon_name())
-print('Ветер: ' + str(weather.get_wind()))
+translate = {'Rostov-on-Don': 'Ростов-на-Дону', 'Moscow': 'Москва'}
+
+def WhatIsCloudness():
+    if 0 <= weather.get_clouds() <= 10:
+        return 'ясная'
+
+    if 10 < weather.get_clouds() <= 30:
+        return 'немного облачная'
+
+    if 30 < weather.get_clouds() <= 70:
+        return 'пасмурная'
+
+    if 70 < weather.get_clouds() <= 100:
+        return 'мрачная'
+
+print('Погода в городе ' + translate[location.get_name()] + ' ' + WhatIsCloudness() + ', температура ' + str(weather.get_temperature('celsius')['temp']) + ' градусов цельсия ' + ' скорость ветра ' + str(weather.get_wind()['speed']) + 'м/с.')
